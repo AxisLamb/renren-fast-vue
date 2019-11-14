4eb5da4e7ea6791a7f7f798e757bce60225ba026
liugaoyang <liugaoyang@girirobot.com>
2019-11-14 10:50:49 +0800

    1. 提交nginx

--- static/config/index-qa.js
+++ static/config/index-qa.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://demo.renren.io/renren-fast';
+  window.SITE_CONFIG['baseUrl'] = 'http://192.168.200.203/renren-fast';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
